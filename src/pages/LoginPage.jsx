675cedd72f2cb6938cb299f13eca5a17c19e1f11
Mikhail Zakharov <mike.zakharov1990@gmail.com>
2024-10-26 17:36:44 +0300

    pull team changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import useUserData from '../store/store';
 
 const LoginPage = () => {
-
-  const userData = useUserData(state=> state.userData);
-  const fetchUserData = useUserData(state=> state.fetchUserData);
-  let data = fetchUserData()
-  console.log(data);
-
   return <div>LoginPage</div>;
 
 };
