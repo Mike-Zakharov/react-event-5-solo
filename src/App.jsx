635e28049f4f3d0abd8c2e3e29a67dc6fb64229d
Maria2721 <mashaivvas@gmail.com>
2024-10-26 13:48:04 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import HelpRequestPage from './pages/HelpRequestPage.jsx';
+import CatalogPage from './pages/CatalogPage.jsx';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.jsx';
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/catalog"
+            element={
+              <ProtectedRoute>
+                <CatalogPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/help-request/:itemId"
+            element={
+              <ProtectedRoute>
+                <HelpRequestPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
