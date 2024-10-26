4afc865dfc5a1699793d11454938138c43b89519
Elvira Krutova <ninelostr@gmail.com>
2024-10-26 19:30:25 +0100

    feat: add user card for profile page

---
+++ src/components/UserInfoCard/UserInfoCard.jsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  Box,
+  CardActions,
+  CardMedia,
+  Divider,
+} from '@mui/material';
+import avatar from '../../assets/avatar.svg';
+
+export const UserInfoCard = () => {
+  return (
+    <Box sx={{ flex: '0 1 20%' }}>
+      <Card
+        variant="outlined"
+        sx={{
+          paddingBlock: 2.5,
+          maxHeight: 420,
+          borderRadius: 1,
+        }}
+      >
+        <CardMedia sx={{ p: '20px 53px' }}>
+          <img src={avatar} alt="" />
+        </CardMedia>
+        <Divider />
+        <CardContent sx={{ mb: '30px', p: 2.5, textAlign: 'left' }}>
+          <Typography variant="h6" sx={{ mb: '10px' }}>
+            Ангелина Фомина
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Статус: Начинающий
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ p: 2.5 }}>
+          <Button variant="outlined" size="large" color="inherit" sx={{ width: '100%' }}>
+            Выйти из аккаунта
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
