976144b94f98342ea231f5869a2ff598bc5f8f97
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-10-26 18:16:31 +0200

    Added redirect on successful registration

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useUserData from '../store/store';
 import AuthForm from '../components/AuthForm/AuthForm';
 import useAuth from '../hooks/useAuth';
 
 const AuthPage = () => {
+  const navigate = useNavigate();
   const { authenticate, loading, error } = useAuth();
   const setAuth = useUserData((state) => state.useAuth);
   const setToken = useUserData((state) => state.useToken);
@@ -11,9 +13,9 @@ const AuthPage = () => {
   const onSubmit = async (formData) => {
     const authData = await authenticate(formData);
     if (authData) {
-      console.log(authData);
       setAuth(authData.auth);
       setToken(authData.token);
+      navigate('/catalogue');
     }
   };
 
