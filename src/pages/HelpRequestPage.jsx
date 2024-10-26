42b5b469f0b301dcaf0c569d05d40faa42f21f42
Mikhail Zakharov <mike.zakharov1990@gmail.com>
2024-10-26 19:21:43 +0300

    build helpRequestPage

--- src/pages/HelpRequestPage.jsx
+++ src/pages/HelpRequestPage.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Button, Icon, Paper, Typography } from '@mui/material';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
 
 const HelpRequestPage = () => {
   const { itemId } = useParams();
 
   return (
     <>
-      {itemId ? <div>HelpRequestPage with itemId:{itemId}</div> : <p>Page item is not present</p>}
+      {itemId ? 
+        <Paper>
+          <Typography variant='h4' component={h4}>Запрос о помощи</Typography>
+          <Paper>
+            <Button>
+              <StarOutlineIcon/>
+              В избранное
+            </Button>
+          </Paper>
+        </Paper>  
+       : <p>Page item is not present</p>}
     </>
   );
 };
