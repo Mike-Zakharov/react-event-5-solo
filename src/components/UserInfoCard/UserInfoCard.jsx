94185cfb4db0893e5b7790d9bfd0d01902dc2e73
Elvira Krutova <ninelostr@gmail.com>
2024-10-27 03:24:05 +0000

    feat: add logout feature

--- src/components/UserInfoCard/UserInfoCard.jsx
+++ src/components/UserInfoCard/UserInfoCard.jsx
@@ -8,9 +8,21 @@ import {
   CardMedia,
   Divider,
 } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../../context/AuthContext';
 import avatar from '../../assets/avatar.svg';
 
 export const UserInfoCard = () => {
+  const { auth, logout } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    if (!!auth) {
+      logout();
+      navigate('/');
+    }
+  };
+
   return (
     <Box sx={{ flex: '0 1 20%' }}>
       <Card
@@ -34,7 +46,13 @@ export const UserInfoCard = () => {
           </Typography>
         </CardContent>
         <CardActions sx={{ p: 2.5 }}>
-          <Button variant="outlined" size="large" color="inherit" sx={{ width: '100%' }}>
+          <Button
+            variant="outlined"
+            size="large"
+            color="inherit"
+            sx={{ width: '100%' }}
+            onClick={handleLogout}
+          >
             Выйти из аккаунта
           </Button>
         </CardActions>
